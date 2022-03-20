5221a248515ac23401e1965d7a020263b18117a8
AkshatGandhi <akshat.ag@somaiya.edu>
2022-03-21 04:03:34 +0530

    Midsem update

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', function (req, res) {
+  res.render('index', {title: 'PeerPad'});
+});
+
+
+
+var srv = app.listen(port, function() {
+	console.log('Listening on '+port)
+})
+
+app.use('/peerjs', require('peer').ExpressPeerServer(srv, {
+	debug: true
+}))
